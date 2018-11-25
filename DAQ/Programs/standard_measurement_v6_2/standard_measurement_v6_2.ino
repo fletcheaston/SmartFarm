95d2d4a249d051252e3b570e21b9fdb4036a3a1c
Fletcher Easton <fletcheaston@gmail.com>
2018-11-24 16:22:58 -0800

    The client side of the text ide is mostly finished. Using localStorage, it transfers the last saved graphic ide stuff to the text ide. Should be easy to extend and add more sensors.

--- DAQ/Programs/standard_measurement_v6_2/standard_measurement_v6_2.ino
+++ DAQ/Programs/standard_measurement_v6_2/standard_measurement_v6_2.ino
@@ -6,34 +6,6 @@
   | |__| / ____ \ |__| |  \ V /| (_) | / /_
   |_____/_/    \_\___\_\   \_/  \___(_)____|
 
-  updated for v6.2 2/15/18
-  DAQ pinout
-
-  Comm to MCU pin D6
-
-  D2 -- PORT A -- SDI12 -- Decagon5TE,...
-  PORT B -- I2C -- RTC,...
-  PORT C -- Analog -- Analog Sensors...
-  D4/D5 -- PORT D -- Resistive -- Watermark200SS,...
-  PORT E -- 1wire -- DS18b20
-  PORT F -- Serial -- GPS,...
-    SIG -- Additional Sensors
-
-  -- For ATMEL Programming
-
-  extended_fuses=0xFE
-  high_fuses=0xDA
-  low_fuses=0xE2
-
-  Sample Text Layout, to easily parse into an excel file. This means a single data string
-  Timestamp batV solV | WM1 WM2 WM3 WM4 | t1 t2 t3 t4 | Dec1: VWC EC tp Dec2: VWC EC tp Dec3: VWC EC tp
-  82317_12:45 4.06v 4.53v  | NA1 NA2 NA3 NA4 | t1 t2 t3 t4 ...
-
-  New Data string per board includes boardID
-  B2 Upload  B2 110_1:13:36 B2 3.88 0.00 B2 1246.315  NA  9942.299  B2 NA NA NA NA
-
-  Can use the MCU com digital 6 pin to finish the upload process
-  ...See Controller Firmware uploadProgram()
 */
 
 #include <SmartFarmMeasureDAQv6_2.h> //choose v6_1 or v6_2
@@ -45,20 +17,10 @@ SmartFarmMeasure smf;
 void setup()
 {
   String BoardID = "FL";
-  int Temperature_Count = 2;
-  String data;
-  String decagon;
   
   smf.finishUp();
   Serial.begin(57600);
-
-  //415AC735 coord
-
   Wire.begin();
-  //uncomment the next line to set RTC time from compiling
-  //when the battery fails this will show the default date and have to be reset
-
-  //smf.setRTCToComputerTime(__DATE__, __TIME__);
 
   //sensor setup functions...
   smf.setupAll();
