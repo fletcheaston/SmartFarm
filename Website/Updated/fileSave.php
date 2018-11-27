10977e1ec552e3d3b62b1eb447d5b5eb6f7a2b21
Fletcher Easton <fletcheaston@gmail.com>
2018-11-26 21:48:40 -0800

    Directory/file testing part 1.

--- Website/Updated/fileSave.php
+++ Website/Updated/fileSave.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
+$dir = "/var/log";
+if ( !file_exists($dir) ) {
+    mkdir ($dir, 0744);
+}
+
+file_put_contents ($dir.'/program.txt', $_POST['program']);
+
 session_start();
 $_SESSION["my_data"] = $_POST['program'];
 echo "data recieved = " . $_POST['program'];
 ?>
-
-</body>
-</html>
